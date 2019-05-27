d5b55befe85f78b2441446318994123802b22727
Alessio Biancalana <dottorblaster@gmail.com>
2019-05-27 22:24:15 +0200

    Update Zuul plug to include :protect_methods option

--- lib/zuul_plug.ex
+++ lib/zuul_plug.ex
@@ -15,11 +15,36 @@ defmodule Zuul.Plug do
     |> Zuul.authenticate(key_file_path)
   end
 
+  defp get_protected_methods(opts) do
+    case List.keytake(opts, :protect_methods, 0) do
+      nil -> nil
+      {{_, value}, _} -> value
+    end
+  end
+
+  defp list_has(element, list) do
+    case Enum.find(list, fn i -> i === element end) do
+      nil -> false
+      _ -> true
+    end
+  end
+
   def call(conn, opts) do
+    protected_methods = get_protected_methods(opts)
+
     cond do
-      get_authorization_header(conn) === nil -> raise(Zuul.MissingAuthorizedHeaderError)
-      check_conn_authorization(conn, opts[:key_file]) === false -> raise(Zuul.KeyError)
-      check_conn_authorization(conn, opts[:key_file]) === true -> conn
+      protected_methods !== nil &&
+          !list_has(conn.method, protected_methods) ->
+        conn
+
+      get_authorization_header(conn) === nil ->
+        raise(Zuul.MissingAuthorizedHeaderError)
+
+      check_conn_authorization(conn, opts[:key_file]) === false ->
+        raise(Zuul.KeyError)
+
+      check_conn_authorization(conn, opts[:key_file]) === true ->
+        conn
     end
   end
 end
