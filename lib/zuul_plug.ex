e95c9040ed42e79a12dfd1676b42849c872298ed
Alessio Biancalana <dottorblaster@gmail.com>
2019-05-27 19:11:47 +0200

    Format and refactor the plug to cond strategy

--- lib/zuul_plug.ex
+++ lib/zuul_plug.ex
@@ -9,17 +9,17 @@ defmodule Zuul.Plug do
     |> List.first()
   end
 
+  defp check_conn_authorization(conn, key_file_path) do
+    conn
+    |> get_authorization_header()
+    |> Zuul.authenticate(key_file_path)
+  end
+
   def call(conn, opts) do
-    if get_authorization_header(conn) === nil, do: raise(Zuul.MissingAuthorizedHeaderError)
-
-    is_ok =
-      conn
-      |> get_authorization_header()
-      |> Zuul.authenticate(opts[:key_file])
-
-    case is_ok do
-      true -> conn
-      false -> raise(Zuul.KeyError)
+    cond do
+      get_authorization_header(conn) === nil -> raise(Zuul.MissingAuthorizedHeaderError)
+      check_conn_authorization(conn, opts[:key_file]) === false -> raise(Zuul.KeyError)
+      check_conn_authorization(conn, opts[:key_file]) === true -> conn
     end
   end
 end
